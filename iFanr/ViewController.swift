0d8cf6f854875af84c66aa33879ba8c32c0c78b8
li125434104 <125434104@qq.com>
2016-08-29 15:35:03 +0800

    Initial Commit

---
+++ iFanr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iFanr
+//
+//  Created by LXJ on 16/8/29.
+//  Copyright © 2016年 LianLuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
